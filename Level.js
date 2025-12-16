22cfea462bead46e7570252b4e4311761a884fce
Beatriz <A41900@alunos.isel.pt>
2025-12-16 22:03:18 +0000

    Add Platform and Level concept

---
+++ Level.js
@@ -0,0 +1,41 @@
+import Platform from "./Platform.js";
+
+export default class Level {
+  constructor(levelNumber, gameEl) {
+    this.gameEl = gameEl;
+    this.platforms = Level.createPlatforms(levelNumber);
+    this.obstacles = [];
+  }
+
+  load() {
+    this.platforms.forEach(p => p.attach(this.gameEl));
+  }
+
+  update(worldX) {
+    this.platforms.forEach(p => p.update(worldX));
+  }
+
+  destroy() {
+    this.platforms.forEach(p => p.el.remove());
+    this.platforms = [];
+  }
+
+  static createPlatforms(levelNumber) {
+    switch (levelNumber) {
+      case 1:
+        return [
+          new Platform(260, 380, 130, 10),
+          new Platform(420, 340, 130, 10),
+          new Platform(600, 300, 10, 200),
+        ];
+
+      default:
+        console.warn("Level não existe:", levelNumber);
+        return [];
+    }
+  }
+  
+
+}
+
+
